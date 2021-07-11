d490d218f4b1dffd0e14e6893a2a4fb11a7aa718
nixnori <nikkinoriega@outlook.com>
2021-07-11 17:32:32 -0400

    Edit API response

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = 8000;
-const properties = require('./data/properties.json');
+const properties = require("./data/properties.json");
 
-app.get('/properties', (req, res) => {
-    res.status(200).json(properties);
+app.get("/properties", (req, res) => {
+	res.status(200).json({ message: "Success.", payload: properties });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+	console.log(`Example app listening on port ${port}!`);
 });
